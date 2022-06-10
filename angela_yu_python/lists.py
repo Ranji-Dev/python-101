e493a403fa50fa9d5b088d680e1d30b921e1bbdb
Ranjith <ranjiranjith8@gmail.com>
2022-06-10 15:43:29 +0530

    angela_yu

---
+++ angela_yu_python/lists.py
@@ -0,0 +1,8 @@
+#states_of_america = ["Delaware","Pennsylvania","New Jersey","Georgia","Connecticut","Massachusetts","Maryland","South Carolina","New Hampshire",
+                     #"New York","North Carolina","Rhode Island","Vermont","Kentucky","Tennessee"]
+
+# print(states_of_america[1])
+# states_of_america.append("Ranjilando")
+# states_of_america.extend(["radhalando","zorolando","luffylando","onami"])
+# states_of_america.insert(2,"fuckoff")
+# print(states_of_america)
