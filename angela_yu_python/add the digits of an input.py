123f2f4e36c9e81d1f31ce3061cfc22a01ad4b1e
Ranjith <ranjiranjith8@gmail.com>
2022-06-10 10:17:21 +0530

    angela_yu

---
+++ angela_yu_python/add the digits of an input.py
@@ -0,0 +1,6 @@
+num = input("Enter any two digit number :\n")
+first_digit = num[0]
+second_digit = num[1]
+
+result = int(first_digit)+int(second_digit)
+print(result)
---
+++ angela_yu_python/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the band generator! ")
+
+city = input("Which city did you grew up in: \n")
+
+pet = input("What is the name of your pet :\n")
+
+print("your band name could be :"+city+" "+ pet)
